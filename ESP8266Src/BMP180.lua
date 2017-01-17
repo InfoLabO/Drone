349182e660a08a6c76a12bc99c552a7030ae2135
TheTiger-PC\TheTiger <gauthier.sornet@gmail.com>
2017-01-17 22:51:06 +0100

    no message

--- ESP8266Src/BMP180.lua
+++ ESP8266Src/BMP180.lua
@@ -29,7 +29,7 @@ end
 
 function bmp180_rdToc_reg()
 	write_reg(0x77,0xF4,0x2E)
-	tmr.delay(4500)
+	tmr.delay(5000)
 	local UT = read_reg16(0x77,0xF6)
 	local X1 = ((UT-AC6)*AC5)/32768
 	local X2 = MC*2048/(X1+MD)
@@ -39,13 +39,16 @@ end
 
 function bmp180_rdPres_reg(oss)
 	write_reg(0x77,0xF4,0x34+bit.lshift(oss,6))
-	tmr.delay(4500)
+	if oss==0 then tmr.delay(5000) end
+	if oss==1 then tmr.delay(8000) end
+	if oss==2 then tmr.delay(14000) end
+	if oss==3 then tmr.delay(26000) end
 	local UP = bit.rshift(read_regU24(0x77,0xF6),(8-oss))
 	local B6 = B5-4000
 	local X1 = (B2*(B6*B6/4096))/2048
 	local X2 = (AC2*B6)/2048
 	local X3 = X1+X2
-	local B3 = ((AC1*4+X3)*bit.lshift(2,oss)+2)/4
+	local B3 = ((AC1*4+X3)*bit.lshift(1,oss)+2)/4
 	X1 = (AC3*B6)/8192
 	X2 = (B1*((B6*B6)/4096))/65536
 	X3 = ((X1+X2)+2)/4
@@ -57,5 +60,9 @@ function bmp180_rdPres_reg(oss)
 	X1 = (X1*3038)/65536
 	X2 = (-7357*p)/65536
 	return p+(X1+X2+3791)/16
+end
+
+function bmp180_calc_alt(sealvlp,rdlvlp)
+    return 44330*(1.0-math.pow(rdlvlp/sealvlp,0.1903))
 end
 
