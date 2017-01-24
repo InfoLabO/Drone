931a502015f3d6ee9ddf8680d385ee8150b8ffad
TheTiger-PC\TheTiger <gauthier.sornet@gmail.com>
2017-01-24 23:17:52 +0100

    no message

--- ESP8266Src/I2C.lua
+++ ESP8266Src/I2C.lua
@@ -1,39 +1,28 @@
 i2c.setup(0, 1, 2, i2c.SLOW)
 
-
-function read_reg(dev_addr, reg_addr)
+function read_reg_n(dev_addr, reg_addr, n)
     i2c.start(0)
     i2c.address(0, dev_addr, i2c.TRANSMITTER)
     i2c.write(0, reg_addr)
     i2c.stop(0)
     i2c.start(0)
     i2c.address(0, dev_addr, i2c.RECEIVER)
-    c = string.byte(i2c.read(0, 1),1)
+    local s = i2c.read(0, n)
     i2c.stop(0)
-    return c
+    return s
+end
+
+function read_reg(dev_addr, reg_addr)
+    return string.byte(read_reg_n(dev_addr,reg_addr,1),1)
 end
 
 function read_regU16(dev_addr, reg_addr)
-    i2c.start(0)
-    i2c.address(0, dev_addr, i2c.TRANSMITTER)
-    i2c.write(0, reg_addr)
-    i2c.stop(0)
-    i2c.start(0)
-    i2c.address(0, dev_addr, i2c.RECEIVER)
-    local s = i2c.read(0, 2)
-    i2c.stop(0)
+    local s = read_reg_n(dev_addr,reg_addr,2)
     return bit.lshift(string.byte(s,1),8)+string.byte(s,2)
 end
 
 function read_regU24(dev_addr, reg_addr)
-    i2c.start(0)
-    i2c.address(0, dev_addr, i2c.TRANSMITTER)
-    i2c.write(0, reg_addr)
-    i2c.stop(0)
-    i2c.start(0)
-    i2c.address(0, dev_addr, i2c.RECEIVER)
-    local s = i2c.read(0, 3)
-    i2c.stop(0)
+    local s = read_reg_n(dev_addr,reg_addr,3)
     return bit.lshift(string.byte(s,1),16)+bit.lshift(string.byte(s,2),8)+string.byte(s,3)
 end
 
--- ESP8266Src/LSM303DLHC.lua
+++ ESP8266Src/LSM303DLHC.lua
@@ -1,2 +1,28 @@
 -- Accel sensor adress = 0x19
+-- Add 0x08 to the reg adresse to get the auto increment
 
+function lsm303dlhc_setRefRate(refrate)
+	write_reg(0x19,0x20,0x07+bit.lshift(refrate,4))
+end
+
+function lsm303dlhc_getRawAccelXYZ()
+	local s = read_reg_n(0x19,0xA8,6);
+	local x = s:byte(1) + bit.lshift(s:byte(2),8)
+	local y = s:byte(3) + bit.lshift(s:byte(4),8)
+	local z = s:byte(5) + bit.lshift(s:byte(6),8)
+	if x>32768 then x = x  - 65536 end
+	if y>32768 then y = y  - 65536 end
+	if z>32768 then z = z  - 65536 end
+    return x,y,z;
+end
+
+function lsm303dlhc_getRawMagnXYZ()
+	local s = read_reg_n(0x19,0x83,6);
+	local x = s:byte(2) + bit.lshift(s:byte(1),8)
+	local y = s:byte(6) + bit.lshift(s:byte(5),8)
+	local z = s:byte(4) + bit.lshift(s:byte(3),8)
+	if x>32768 then x = x  - 65536 end
+	if y>32768 then y = y  - 65536 end
+	if z>32768 then z = z  - 65536 end
+    return x,y,z;
+end
